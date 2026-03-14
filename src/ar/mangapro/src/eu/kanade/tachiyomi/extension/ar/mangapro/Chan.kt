f5354f0e93ef53195a83477aa85bb5beca279e67
Abdelkaderhachimi <35611391+Abdelkaderhachimi@users.noreply.github.com>
2026-03-14 03:21:47 +0100

    Chan.kt

--- src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/Chan.kt
+++ src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/Chan.kt
@@ -50,10 +50,10 @@ import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.SecretKeySpec
 import kotlin.io.encoding.Base64
 
-class ProChan : HttpSource() {
-    override val name = "ProChan"
+class procomic : HttpSource() {
+    override val name = "ProComic"
     override val lang = "ar"
-    private val domain = "prochan.net"
+    private val domain = "procomic.net"
     override val baseUrl = "https://$domain"
     override val supportsLatest = true
     override val versionId = 5
