434cc9222709d10d1cf8b9bd8631386b1044d478
Abdelkaderhachimi <35611391+Abdelkaderhachimi@users.noreply.github.com>
2026-04-02 10:53:44 +0100

    Update UrlActivity with manifest and permissions

--- src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/UrlActivity.kt
+++ src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/UrlActivity.kt
@@ -1,29 +1,53 @@
-package eu.kanade.tachiyomi.extension.ar.mangapro
+<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://android.com"
+    package="eu.kanade.tachiyomi.extension.ar.mangapro">
 
-import android.app.Activity
-import android.content.ActivityNotFoundException
-import android.content.Intent
-import android.os.Bundle
-import android.util.Log
-import kotlin.system.exitProcess
+    <!-- السماح للإضافة بالاتصال بالإنترنت -->
+    <uses-permission android:name="android.permission.INTERNET" />
+    <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
 
-class UrlActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    <application
+        android:allowBackup="false"
+        android:icon="@mipmap/ic_launcher"
+        android:label="Tachiyomi: ProComic (Custom)">
 
-        val mainIntent = Intent().apply {
-            action = "eu.kanade.tachiyomi.SEARCH"
-            putExtra("query", intent.data.toString())
-            putExtra("filter", packageName)
-        }
+        <!-- تعريف الـ UrlActivity لفتح الروابط من المتصفح مباشرة -->
+        <activity
+            android:name=".UrlActivity"
+            android:exported="true"
+            android:label="ProComic Path Intent"
+            android:theme="@android:style/Theme.NoDisplay">
+            
+            <intent-filter>
+                <action android:name="android.intent.action.VIEW" />
+                <category android:name="android.intent.category.DEFAULT" />
+                <category android:name="android.intent.category.BROWSABLE" />
+                
+                <!-- بروتوكول الموقع -->
+                <data android:scheme="http" />
+                <data android:scheme="https" />
+                
+                <!-- رابط الموقع الرئيسي -->
+                <data android:host="procomic.pro" />
+                
+                <!-- مسارات الروابط التي سيتم اعتراضها (مانهوا، فصل) -->
+                <data android:pathPrefix="/series/" />
+                <data android:pathPrefix="/manga/" />
+            </intent-filter>
+        </activity>
 
-        try {
-            startActivity(mainIntent)
-        } catch (e: ActivityNotFoundException) {
-            Log.e("ProChan", "Unable to launch activity", e)
-        }
+        <!-- تعريف الفئة الرئيسية للإضافة (Metadata) ليتعرف عليها تاتشيومي -->
+        <meta-data
+            android:name="tachiyomi.extension.class"
+            android:value=".ProComic" />
+        
+        <meta-data
+            android:name="tachiyomi.extension.version"
+            android:value="1" />
+            
+        <meta-data
+            android:name="tachiyomi.extension.nsfw"
+            android:value="0" /> <!-- 0 للمحتوى العام، 1 للمحتوى البالغين -->
 
-        finish()
-        exitProcess(0)
-    }
-}
+    </application>
+</manifest>
