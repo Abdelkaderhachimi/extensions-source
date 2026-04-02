22df4c55bcc48c56ef71fe014baed20046cf0799
Abdelkaderhachimi <35611391+Abdelkaderhachimi@users.noreply.github.com>
2026-04-02 11:34:18 +0100

    Refactor UrlActivity to Kotlin and remove XML manifest
    Removed XML manifest content and replaced it with Kotlin code for UrlActivity.

--- src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/UrlActivity.kt
+++ src/ar/mangapro/src/eu/kanade/tachiyomi/extension/ar/mangapro/UrlActivity.kt
@@ -1,53 +1,34 @@
-<?xml version="1.0" encoding="utf-8"?>
-<manifest xmlns:android="http://android.com"
-    package="eu.kanade.tachiyomi.extension.ar.mangapro">
+package eu.kanade.tachiyomi.extension.ar.mangapro
 
-    <!-- السماح للإضافة بالاتصال بالإنترنت -->
-    <uses-permission android:name="android.permission.INTERNET" />
-    <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import kotlin.system.exitProcess
 
-    <application
-        android:allowBackup="false"
-        android:icon="@mipmap/ic_launcher"
-        android:label="Tachiyomi: ProComic (Custom)">
+class UrlActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val pathSegments = intent?.data?.pathSegments
+        if (pathSegments != null && pathSegments.size >= 2) {
+            val mainIntent = Intent().apply {
+                action = "eu.kanade.tachiyomi.SEARCH"
+                putExtra("query", pathSegments.last())
+                putExtra("filter", packageName)
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
 
-        <!-- تعريف الـ UrlActivity لفتح الروابط من المتصفح مباشرة -->
-        <activity
-            android:name=".UrlActivity"
-            android:exported="true"
-            android:label="ProComic Path Intent"
-            android:theme="@android:style/Theme.NoDisplay">
-            
-            <intent-filter>
-                <action android:name="android.intent.action.VIEW" />
-                <category android:name="android.intent.category.DEFAULT" />
-                <category android:name="android.intent.category.BROWSABLE" />
-                
-                <!-- بروتوكول الموقع -->
-                <data android:scheme="http" />
-                <data android:scheme="https" />
-                
-                <!-- رابط الموقع الرئيسي -->
-                <data android:host="procomic.pro" />
-                
-                <!-- مسارات الروابط التي سيتم اعتراضها (مانهوا، فصل) -->
-                <data android:pathPrefix="/series/" />
-                <data android:pathPrefix="/manga/" />
-            </intent-filter>
-        </activity>
+            try {
+                startActivity(mainIntent)
+            } catch (e: ActivityNotFoundException) {
+                Log.e("UrlActivity", e.toString())
+            }
+        } else {
+            Log.e("UrlActivity", "Could not parse URI from intent $intent")
+        }
 
-        <!-- تعريف الفئة الرئيسية للإضافة (Metadata) ليتعرف عليها تاتشيومي -->
-        <meta-data
-            android:name="tachiyomi.extension.class"
-            android:value=".ProComic" />
-        
-        <meta-data
-            android:name="tachiyomi.extension.version"
-            android:value="1" />
-            
-        <meta-data
-            android:name="tachiyomi.extension.nsfw"
-            android:value="0" /> <!-- 0 للمحتوى العام، 1 للمحتوى البالغين -->
-
-    </application>
-</manifest>
+        finish()
+        exitProcess(0)
+    }
+}
